2e97332b940d75782eed897c40028dbf8be54884
Rizzi <rizzi@perihelion>
2023-10-11 01:40:01 -0700

    created hw1_2a, hw1_2b

---
+++ MAE-144/HW1/hw1_2b.m
@@ -0,0 +1,9 @@
+clear; clc;
+
+a = RR_poly([-1 1 -3 3 -6 6],1); 
+b = RR_poly([-2 2 -5 5],1); 
+f = RR_poly([1 1 3 3 6 6 20 20 20 20 20 20],1);
+
+[x,y] = RR_diophantine(a,b,f)
+test = trim(a*x+b*y)
+residual = norm(f - test)
